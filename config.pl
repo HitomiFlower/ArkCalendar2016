17c2722e2bfe7aa426cfc9e7d86b53b211cbe2cb
HitomiFlower <anheikuaiyun001@gmail.com>
2021-01-19 14:10:17 +0900

    7日目

--- config.pl
+++ config.pl
@@ -1,7 +1,11 @@
 my $home = Jobeet::Models->get('home');
 
 return {
-    database => [
+    default_view => 'MT',
+    active_days  => 30,
+    max_jobs_on_homepage => 10,
+
+    database     => [
         'dbi:SQLite:' . $home->file('database.db'), '', '',
         {
             sqlite_unicode => 1,
