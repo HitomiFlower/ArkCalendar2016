650ac71cfa2cc7d21e1912f05bd7a7adb0c450d4
HitomiFlower <anheikuaiyun001@gmail.com>
2021-01-19 12:02:23 +0900

    6日目(ルーティング)

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -2,11 +2,41 @@ package Jobeet::Controller::Job;
 use Ark 'Controller';
 
 use Jobeet::Models;
-has '+namespace' => default => 'job';
+has '+namespace' => default => 'job'; # URL対応
 
-sub index : Path {
+# /boo/bar
+# sub bar :Path('bar') メソッド名は関係ない
+# /boo/の場合Pathに何も指定しない
+# Argsにパラメータ受け取る数を指定. 0の場合が省略できる
+sub index :Path {
     my ($self, $c) = @_;
     $c->stash->{jobs} = models('Schema::Job');
+}
+
+# job/{job_token}(detail)
+sub show :Path :Args(1) {
+    my ($self, $c, $job_token) = @_;
+}
+
+# /job/create (new)
+sub create :Local {
+    my ($self, $c) = @_;
+}
+
+
+sub job :Chained('/) :PathPart :CapureArgs(1) {
+    my ($self, $c, $job_token) = @_;
+    $c->stash->{job_token} = $job_token;
+}
+
+# /job/{job_token}/edit
+sub edit :Chained('job) :PathPart :Args(0) {
+    my ($self, $c) = @_;
+}
+
+# /job/{job_token}/delete
+sub delete :Chained('job) :PathPart :Args(0) {
+    my ($self, $c) = @_;
 }
 
 __PACKAGE__->meta->make_immutable;
